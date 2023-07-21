7f97c34ff9cd023bd33c6c27a07d4a10d79d5b4b
leoduchateau26 <leoduchateau26000@outlook.fr>
2023-07-21 11:46:41 +0200

    premiere modif

--- OneDrive/Bureau/youpi/circuit/Main.java
+++ OneDrive/Bureau/youpi/circuit/Main.java
@@ -1,10 +1,27 @@
+
 package circuit;
+import java.util.Scanner;
+
+
 
 public class Main {
 
 public static void main(String[] args) {
+
+
+// création des voitures    
     
-voiture voiture1 = new voiture(270, renault, 2007);
+voiture voiture1 = new voiture(270, "renault", 2007);
+voiture voiture2 = new voiture(310, "aston", 1998);
+voiture voiture3 = new voiture(150, "Ferrari", 2002);
+
+// création des pilotes
+
+pilote leo = new pilote("leo", 26, 3);
+pilote brice = new pilote("Brice", 26, 8);
+pilot thomas = new pilote("Thomas", 33, 6);
+
+// c'est l'heure des conditions de la course pour gagner
 
 
 
